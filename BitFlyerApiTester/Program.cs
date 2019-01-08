fe1e54b9b49a75bffab99ce413c9557712b58ecd
supermomonga <hi@supermomonga.com>
2019-01-08 20:21:02 +0900

    init

--- BitFlyerApiTester/Program.cs
+++ BitFlyerApiTester/Program.cs
@@ -1,15 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
 
 namespace BitFlyerApiTester
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+
+            Console.WriteLine("API Key を入力してください：");
+            var key = Console.ReadLine().Trim();
+
+            Console.WriteLine("API Secret を入力してください：");
+            var secret = Console.ReadLine().Trim();
+
+            Hr();
+
+            Console.WriteLine($"Key:Secret = {key}:{secret}");
+
+            Hr();
+
+            Console.WriteLine("API呼び出しを開始します");
+
+            Hr();
+
+            using (var hc = new HttpClient())
+            {
+                var path = "/v1/me/getpermissions";
+                var req = new HttpRequestMessage(HttpMethod.Get, $"https://api.bitflyer.com{path}");
+
+                var timestamp = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString();
+                var content = timestamp + req.Method + path;
+                var hash = new HMACSHA256(Encoding.UTF8.GetBytes(secret)).ComputeHash(Encoding.UTF8.GetBytes(content));
+
+                req.Headers.TryAddWithoutValidation("ACCESS-KEY", key);
+                req.Headers.TryAddWithoutValidation("ACCESS-TIMESTAMP", timestamp);
+                req.Headers.TryAddWithoutValidation("ACCESS-SIGN", hash.ToHexString());
+
+                var res = await hc.SendAsync(req).ConfigureAwait(false);
+                var json = await res.Content.ReadAsStringAsync();
+
+                Console.WriteLine("HTTP Status code:");
+                Console.WriteLine(res.StatusCode);
+                Console.WriteLine("Response body:");
+                Console.WriteLine(json);
+            }
+
+            Hr();
+
+            Console.WriteLine("API呼び出しを完了しました");
+
+            Hr();
+
+            Console.WriteLine("Enter(CR) を打鍵で終了します");
+            Console.ReadLine();
+        }
+
+        private static void Hr()
+        {
+            Console.WriteLine();
+            Console.WriteLine("------------------------------");
+            Console.WriteLine();
         }
     }
 }
---
+++ BitFlyerApiTester/StringHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BitFlyerApiTester
+{
+    public static class StringHelper
+    {
+        public static string ToHexString(this byte[] data)
+            => data.Aggregate(new StringBuilder(data.Length * 2), (s, b) => s.AppendHex(b >> 4).AppendHex(b & 15)).ToString();
+
+        private static StringBuilder AppendHex(this StringBuilder sb, int hb)
+            => sb.Append((char)(hb < 10 ? '0' + hb : ('a' + hb - 10)));
+    }
+}
